92a58c052e40d931953f686ffcc3927be94d1ad8
Lukas <hallo@lukas-schardt.de>
2020-07-03 10:21:17 +0200

    #6: Fixes child list mutation event by listening to the after update hook

--- test/unit/child-list-mutation.test.js
+++ test/unit/child-list-mutation.test.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-unused-expressions */
-import { fixture, defineCE, assert, nextFrame } from '@open-wc/testing';
+import { fixture, defineCE, assert, nextFrame, oneEvent } from '@open-wc/testing';
 import { BaseElement } from 'src/BaseElement';
 
 const tag = defineCE(
@@ -7,6 +7,10 @@ const tag = defineCE(
 		constructor() {
 			super({ deferUpdate: false });
 			this.updateCount = 0;
+		}
+
+		afterUpdate() {
+			this.dispatch('afterUpdate', null, true);
 		}
 
 		registerEventsAndRefs() {
@@ -17,18 +21,18 @@ const tag = defineCE(
 );
 
 describe('child-list-mutation', () => {
-	// it('re-registers events and refs after a child node has been added', async () => {
-	// 	const el = await fixture(`<${tag}></${tag}>`);
-	// 	el.updateCount = 0;
-	// 	el.innerHTML = `<span>i am nested</span>`;
-	// 	await nextFrame();
-	// 	assert.equal(el.updateCount, 1);
-	// });
-	// it('re-registers events and refs after a child node has been removed', async () => {
-	// 	const el = await fixture(`<${tag}><span id="remove"></span></${tag}>`);
-	// 	el.updateCount = 0;
-	// 	el.removeChild(el.querySelector('#remove'));
-	// 	await nextFrame();
-	// 	assert.equal(el.updateCount, 1);
-	// });
+	it('re-registers events and refs after a child node has been added', async () => {
+		const el = await fixture(`<${tag}></${tag}>`);
+		el.updateCount = 0;
+		el.innerHTML = `<span>i am nested</span>`;
+		await oneEvent(el, 'afterUpdate');
+		assert.equal(el.updateCount, 1);
+	});
+	it('re-registers events and refs after a child node has been removed', async () => {
+		const el = await fixture(`<${tag}><span id="remove"></span></${tag}>`);
+		el.updateCount = 0;
+		el.removeChild(el.querySelector('#remove'));
+		await oneEvent(el, 'afterUpdate');
+		assert.equal(el.updateCount, 1);
+	});
 });
