f537c7f95429c7e02eee8c842b49a0c7142528f4
eddyloewen <mail@eddyloewen.com>
2020-10-14 16:28:26 +0000

    Deploying to feature/dist-bundle from  @ 13bb7bcb45c9f0b146545932bf8cdf8d88a397f9 🚀

--- index.js
+++
@@ -1,8 +0,0 @@
-import { BaseElement } from './src/BaseElement';
-import { StyledElement } from './src/StyledElement';
-import { TemplateElement, html } from './src/TemplateElement';
-import { toString } from './src/util/toString';
-import { defineElement } from './src/util/defineElement';
-import { i18n } from './src/util/i18n';
-
-export { BaseElement, StyledElement, TemplateElement, html, toString, defineElement, i18n };
