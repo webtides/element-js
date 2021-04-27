733c5eebe70dde885e9a8b3dfd7ccfd7b0564969
Lukas Schardt <lukas@buerozimmer.de>
2021-04-27 08:59:33 +0200

    Made the html literal more object oriented and add a unsafeHTML directive.

--- src/util/render.js
+++ src/util/render.js
@@ -1,6 +1,6 @@
 const convertStringToHTML = (templateString) => {
 	const parser = new DOMParser();
-	const document = parser.parseFromString(templateString, 'text/html');
+	const document = parser.parseFromString(`${templateString}`, 'text/html');
 	return document.body;
 };
 
@@ -253,43 +253,4 @@ const render = (template, target) => {
 	};
 };
 
-const encodeAttribute = (attribute, preserveCR = false) => {
-	preserveCR = preserveCR ? '&#13;' : '\n';
-	return `${attribute}`
-		.replace(/&/g, '&amp;') /* This MUST be the 1st replacement. */
-		.replace(/'/g, '&apos;') /* The 4 other predefined entities, required. */
-		.replace(/"/g, '&quot;')
-		.replace(/</g, '&lt;')
-		.replace(/>/g, '&gt;')
-		.replace(/\r\n/g, preserveCR) /* Must be before the next replacement. */
-		.replace(/[\r\n]/g, preserveCR);
-};
-
-const html = function (strings, ...keys) {
-	let result = [];
-	for (let i = 0; i < strings.length; i++) {
-		result.push(strings[i]);
-
-		if (i < keys.length) {
-			if (typeof keys[i] === 'string') {
-				// We are between two " ", so we are probably dealing with an attribute.
-				if (
-					strings[i + 1] &&
-					(strings[i].endsWith('"') || strings[i].endsWith("'")) &&
-					(strings[i + 1].startsWith('"') || strings[i + 1].startsWith("'"))
-				) {
-					result.push(encodeAttribute(keys[i]));
-				} else {
-					result.push(keys[i]);
-				}
-			} else if (Array.isArray(keys[i])) {
-				result.push(keys[i].join(''));
-			} else {
-				result.push(keys[i]?.toString());
-			}
-		}
-	}
-
-	return result.join('');
-};
-export { html, render };
+export { render };
