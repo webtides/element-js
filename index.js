cf5a37653d508382fd6813326ed0aea0304dc153
Lukas Schardt <lukas@buerozimmer.de>
2021-12-15 11:08:14 +0100

    Remove extra exports

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 import { BaseElement } from './src/BaseElement';
 import { StyledElement } from './src/StyledElement';
-import { TemplateElement, html, unsafeHTML, attr } from './src/TemplateElement';
+import { TemplateElement, html } from './src/TemplateElement';
 import { toString } from './src/util/toString';
 import { defineElement } from './src/util/defineElement';
 import { i18n } from './src/util/i18n';
 
-export { BaseElement, StyledElement, TemplateElement, html, unsafeHTML, attr, toString, defineElement, i18n };
+export { BaseElement, StyledElement, TemplateElement, html, toString, defineElement, i18n };
