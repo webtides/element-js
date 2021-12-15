e8ffeb994bb538d60e0a93e41b63d5d0a979c00e
Lukas Schardt <lukas@buerozimmer.de>
2021-12-15 11:24:26 +0100

    Add entrypoint for vanilla renderer

---
+++ src/renderer/vanilla/index.js
@@ -0,0 +1,8 @@
+import { BaseElement } from '../../BaseElement';
+import { StyledElement } from '../../StyledElement';
+import { TemplateElement, html, unsafeHTML, attr } from './TemplateElement';
+import { toString } from '../../util/toString';
+import { defineElement } from '../../util/defineElement';
+import { i18n } from '../../util/i18n';
+
+export { BaseElement, StyledElement, TemplateElement, html, unsafeHTML, attr, toString, defineElement, i18n };
