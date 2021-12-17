583a2b128a832977077d3c0ec54c9ce451a6d985
Markus Hesper <mh@19f.de>
2021-12-17 11:07:11 +0100

    feat(tests): change prettier config to use spaces instead of tabs #27

--- test/unit/template-rendering.test.js
+++ test/unit/template-rendering.test.js
@@ -3,68 +3,68 @@ import { fixture, defineCE, assert, nextFrame } from '@open-wc/testing';
 import { TemplateElement, html } from '../../src/TemplateElement';
 
 const lightTag = defineCE(
-	class extends TemplateElement {
-		template() {
-			return html` <div>light content</div> `;
-		}
-	},
+    class extends TemplateElement {
+        template() {
+            return html` <div>light content</div> `;
+        }
+    },
 );
 
 const shadowTag = defineCE(
-	class extends TemplateElement {
-		constructor() {
-			super({ shadowRender: true });
-		}
+    class extends TemplateElement {
+        constructor() {
+            super({ shadowRender: true });
+        }
 
-		template() {
-			return html` <div>shadow content</div> `;
-		}
-	},
+        template() {
+            return html` <div>shadow content</div> `;
+        }
+    },
 );
 
 const deferTag = defineCE(
-	class extends TemplateElement {
-		constructor() {
-			super({ deferUpdate: true });
-		}
+    class extends TemplateElement {
+        constructor() {
+            super({ deferUpdate: true });
+        }
 
-		template() {
-			return html` <div>deferred content</div> `;
-		}
-	},
+        template() {
+            return html` <div>deferred content</div> `;
+        }
+    },
 );
 
 const noHtmlTag = defineCE(
-	class extends TemplateElement {
-		template() {
-			return `<div>no html template result content</div>`;
-		}
-	},
+    class extends TemplateElement {
+        template() {
+            return `<div>no html template result content</div>`;
+        }
+    },
 );
 
 describe('template-rendering', () => {
-	it('renders template in light dom by default', async () => {
-		const el = await fixture(`<${lightTag}></${lightTag}>`);
-		assert.isNull(el.shadowRoot);
-		assert.lightDom.equal(el, '<div>light content</div>');
-	});
+    it('renders template in light dom by default', async () => {
+        const el = await fixture(`<${lightTag}></${lightTag}>`);
+        assert.isNull(el.shadowRoot);
+        assert.lightDom.equal(el, '<div>light content</div>');
+    });
 
-	it('can render template in shadow dom by setting shadowRender: true via constructor options', async () => {
-		const el = await fixture(`<${shadowTag}></${shadowTag}>`);
-		assert.isNotNull(el.shadowRoot);
-		assert.shadowDom.equal(el, '<div>shadow content</div>');
-	});
+    it('can render template in shadow dom by setting shadowRender: true via constructor options', async () => {
+        const el = await fixture(`<${shadowTag}></${shadowTag}>`);
+        assert.isNotNull(el.shadowRoot);
+        assert.shadowDom.equal(el, '<div>shadow content</div>');
+    });
 
-	it('can defer rendering template by setting deferUpdate: true via constructor options', async () => {
-		const el = await fixture(`<${deferTag}></${deferTag}>`);
-		assert.equal(el.innerHTML.trim(), '');
-		assert.lightDom.equal(el, '');
-		await el.requestUpdate();
-		assert.lightDom.equal(el, '<div>deferred content</div>');
-	});
+    it('can defer rendering template by setting deferUpdate: true via constructor options', async () => {
+        const el = await fixture(`<${deferTag}></${deferTag}>`);
+        assert.equal(el.innerHTML.trim(), '');
+        assert.lightDom.equal(el, '');
+        await el.requestUpdate();
+        assert.lightDom.equal(el, '<div>deferred content</div>');
+    });
 
-	it('can render standard strings as template instead of html template results', async () => {
-		const el = await fixture(`<${noHtmlTag}></${noHtmlTag}>`);
-		assert.lightDom.equal(el, '<div>no html template result content</div>');
-	});
+    it('can render standard strings as template instead of html template results', async () => {
+        const el = await fixture(`<${noHtmlTag}></${noHtmlTag}>`);
+        assert.lightDom.equal(el, '<div>no html template result content</div>');
+    });
 });
