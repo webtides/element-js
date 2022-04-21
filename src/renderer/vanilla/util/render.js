9f7f98ace345b6330fde25af677d992e108f73cb
Eddy Löwen <mail@eddyloewen.com>
2022-04-21 09:37:34 +0200

    improves renderer code, adds types and better naming

--- src/renderer/vanilla/util/render.js
+++ src/renderer/vanilla/util/render.js
@@ -1,25 +1,29 @@
 const _cachedTemplateElements = {};
 
-const convertStringToHTML = (templateString) => {
+/**
+ * Parses the given template string and returns a real DOM element.
+ * @param {string} template
+ * @returns {HTMLElement}
+ */
+const convertStringToHTML = (template) => {
 	const parser = new DOMParser();
-	const document = parser.parseFromString(`<main>${templateString}</main>`, 'text/html');
-
+	const document = parser.parseFromString(`<main>${template}</main>`, 'text/html');
 	return document.querySelector('main');
 };
 
 /**
- * Diff the attributes on the target DOM node versus the template DOM node
- * @param  {Element} template The new template
- * @param  {Element} target The existing DOM node
+ * Diff the attributes for a live DOM element against a template DOM element
+ * @param  {Element} templateElement The new template
+ * @param  {Element} domElement The existing DOM node
  */
-const diffAttributes = function (template, target) {
+const diffAttributes = function (templateElement, domElement) {
 	const templateAttributes = {};
-	for (const attribute of Array.from(template.attributes)) {
+	for (const attribute of Array.from(templateElement.attributes)) {
 		templateAttributes[attribute.name] = attribute.value;
 	}
 
 	const targetAttributes = {};
-	for (const attribute of Array.from(target.attributes)) {
+	for (const attribute of Array.from(domElement.attributes)) {
 		targetAttributes[attribute.name] = attribute.value;
 	}
 
@@ -28,19 +32,24 @@ const diffAttributes = function (template, target) {
 	// remove attributes
 	for (const attributeName of Object.keys(targetAttributes)) {
 		if (!templateAttributes[attributeName]) {
-			target.removeAttribute(attributeName);
+			domElement.removeAttribute(attributeName);
 		}
 	}
 
 	// add/update attributes
 	for (const attributeName of Object.keys(templateAttributes)) {
 		// TODO: handle attributes with "." aka properties...
-		target.setAttribute(attributeName, templateAttributes[attributeName] || '');
+		domElement.setAttribute(attributeName, templateAttributes[attributeName] || '');
 	}
 };
 
+/**
+ * Checks whether the given element or node is a TemplateElement or not
+ * @param {Element|Node} element
+ * @returns {boolean}
+ */
 const isTemplateElement = (element) => {
-	const tagName = element.node?.tagName?.toLowerCase() ?? false;
+	const tagName = element.tagName?.toLowerCase() ?? false;
 	if (!tagName || !tagName.includes('-')) {
 		return false;
 	}
@@ -60,19 +69,19 @@ const isTemplateElement = (element) => {
 };
 
 /**
- * Diff the target DOM node versus the template DOM node
- * @param {Node} template
- * @param {Node} target
+ * Diff a live DOM node against a template DOM node
+ * @param {Node} templateNode
+ * @param {Node} domNode
  */
-const diff = function (template, target) {
-	const templateChildNodes = [...template.childNodes];
-	const targetChildNodes = [...target.childNodes];
+const diff = function (templateNode, domNode) {
+	const domChildNodes = [...domNode.childNodes];
+	const templateChildNodes = [...templateNode.childNodes];
 
 	// TODO: what about plainlySetInnerHTML ?!
 	// TODO: what about SVGs ?! do they need special handling?!
 
 	// If extra elements in target, add dummy elements to template so that the length will match
-	let count = targetChildNodes.length - templateChildNodes.length;
+	let count = domChildNodes.length - templateChildNodes.length;
 	if (count > 0) {
 		for (let index = count; index > 0; index--) {
 			templateChildNodes.push(document.createElement('delete-me'));
@@ -80,43 +89,42 @@ const diff = function (template, target) {
 	}
 
 	// Diff each node in the template child nodes array
-	const length = templateChildNodes.length;
-	for (let index = 0; index < length; index++) {
+	for (let index = 0; index < templateChildNodes.length; index++) {
 		const templateNode = templateChildNodes[index];
-		const targetNode = targetChildNodes[index];
+		const domNode = domChildNodes[index];
 
 		// If template node is dummy node, remove node in target
 		if (templateNode.tagName === 'DELETE-ME') {
-			targetNode.parentNode.removeChild(targetNode);
+			domNode.parentNode.removeChild(domNode);
 			continue;
 		}
 
 		// If target node doesn't exist, create it
-		if (!targetChildNodes[index]) {
-			target.appendChild(templateNode);
+		if (!domChildNodes[index]) {
+			domNode.appendChild(templateNode);
 			continue;
 		}
 
 		// If target node is equal to the template node, don't do anything
-		if (targetNode.isEqualNode(templateNode)) {
+		if (domNode.isEqualNode(templateNode)) {
 			continue;
 		}
 
 		// If node type is not the same, replace it with the template node
-		if (templateNode.nodeType !== targetNode.nodeType) {
-			targetNode.parentNode.replaceChild(templateNode, targetNode);
+		if (templateNode.nodeType !== domNode.nodeType) {
+			domNode.parentNode.replaceChild(templateNode, domNode);
 			continue;
 		}
 
 		// If attributes are different, update them
 		if (templateNode.nodeType === 1) {
-			diffAttributes(templateNode, targetNode);
+			diffAttributes(templateNode, domNode);
 		}
 
 		// If content is different, update it
-		if (targetNode.nodeType === 3) {
-			if (targetNode.textContent !== templateNode.textContent) {
-				targetNode.textContent = templateNode.textContent;
+		if (domNode.nodeType === 3) {
+			if (domNode.textContent !== templateNode.textContent) {
+				domNode.textContent = templateNode.textContent;
 				continue;
 			}
 		}
@@ -128,35 +136,40 @@ const diff = function (template, target) {
 		// }
 
 		// If target should be empty, remove all child nodes
-		if (targetNode.hasChildNodes() && !templateNode.hasChildNodes()) {
-			targetNode.replaceChildren();
+		if (domNode.hasChildNodes() && !templateNode.hasChildNodes()) {
+			domNode.replaceChildren();
 			continue;
 		}
 
 		// If target is empty but shouldn't be, add child nodes
-		if (!targetNode.hasChildNodes() && templateNode.hasChildNodes()) {
-			targetNode.replaceChildren(...templateNode.childNodes);
+		if (!domNode.hasChildNodes() && templateNode.hasChildNodes()) {
+			domNode.replaceChildren(...templateNode.childNodes);
 			continue;
 		}
 
 		// If there are existing child elements that need to be modified, diff them
 		if (templateNode.hasChildNodes()) {
 			if (!isTemplateElement(templateNode)) {
-				diff(templateNode, targetNode);
+				diff(templateNode, domNode);
 			}
 		}
 	}
 };
 
-const render = (template, target) => {
+/**
+ * Render a template string into the given DOM node
+ * @param {string} template
+ * @param {Node} domNode
+ */
+const render = (template, domNode) => {
 	console.time('render');
 
 	console.time('convertStringToHTML');
-	const domTemplate = convertStringToHTML(template);
+	const templateNode = convertStringToHTML(template);
 	console.timeEnd('convertStringToHTML');
 
 	console.time('diff');
-	diff(domTemplate, target);
+	diff(templateNode, domNode);
 	console.timeEnd('diff');
 
 	console.timeEnd('render');
