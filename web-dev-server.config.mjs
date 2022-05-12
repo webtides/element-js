d25e73231251326800f63f9c1858d720595d64c2
Eddy Löwen <mail@eddyloewen.com>
2022-05-12 17:03:13 +0200

    cleans up web-dev-server config file

--- web-dev-server.config.mjs
+++ web-dev-server.config.mjs
@@ -1,18 +1,6 @@
-// import { fromRollup } from '@web/dev-server-rollup';
-// import { babel as rollupBabel } from '@rollup/plugin-babel';
-
-// const babel = fromRollup(rollupBabel);
-
 export default {
 	nodeResolve: true,
 	watch: true,
 	rootDir: './examples',
-	plugins: [
-		// babel({
-		// 	babelHelpers: 'bundled',
-		// 	plugins: [
-		// 		['@babel/plugin-proposal-decorators', { version: '2021-12' }],
-		// 	],
-		// }),
-	],
+	plugins: [],
 };
