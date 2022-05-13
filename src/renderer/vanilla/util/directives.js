daea4db050f8134159e87f623f1b0b14305040fd
Eddy Löwen <mail@eddyloewen.com>
2022-05-13 21:42:55 +0200

    adds examples for directives

--- src/renderer/vanilla/util/directives.js
+++ src/renderer/vanilla/util/directives.js
@@ -29,9 +29,9 @@ const styleMap = (map) => {
 /**
  * Renders one of two template parts based on a condition.
  * @param {boolean} condition
- * @param {TemplatePart} trueCase
- * @param {TemplatePart} falseCase
- * @returns {TemplatePart}
+ * @param {TemplateLiteral | string} trueCase
+ * @param {TemplateLiteral | string} [falseCase]
+ * @returns {TemplateLiteral | string}
  */
 const when = (condition, trueCase, falseCase) => {
 	return condition ? trueCase : falseCase;
@@ -40,22 +40,17 @@ const when = (condition, trueCase, falseCase) => {
 /**
  * Chooses and evaluates a template part from a map of cases based on matching the given value to a case.
  * @param {string} value
- * @param {Object.<string, TemplatePart>} cases
- * @param {TemplatePart} defaultCase
- * @returns {TemplatePart}
+ * @param {Object.<string, TemplateLiteral | string>} cases
+ * @param {TemplateLiteral | string} [defaultCase]
+ * @returns {TemplateLiteral | string}
  */
 const choose = (value, cases, defaultCase) => {
 	return cases[value] || defaultCase;
 };
 
-/**
- * Returns an iterable containing the result of calling callback(item) on each item in the list.
- * @param items
- * @param callback
- * @returns {TemplatePart}
- */
-const map = (items, callback) => {
-	return items.map(callback);
-};
+// what more?
+// ifDefined?
+// booleanAttribute?
+// ref?
 
-export { classMap, styleMap, when, choose, map };
+export { classMap, styleMap, when, choose };
