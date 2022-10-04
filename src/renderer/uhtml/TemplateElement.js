ed99b818be2a3c74b4ac805f971acb1c90e0001f
Eddy Löwen <mail@eddyloewen.com>
2022-10-04 16:07:57 +0200

    wip

---
+++ src/renderer/uhtml/TemplateElement.js
@@ -0,0 +1,52 @@
+import { StyledElement } from '../../StyledElement';
+import { html, spreadAttributes, unsafeHTML } from './util/html';
+import { render } from './util/render';
+export { i18n } from '../../util/i18n';
+
+class TemplateElement extends StyledElement {
+	static _$templateElement$ = true;
+
+	constructor(options) {
+		super({
+			deferUpdate: false,
+			shadowRender: false,
+			styles: [],
+			adoptGlobalStyles: true,
+			mutationObserverOptions: {
+				childList: false,
+			},
+			...options,
+		});
+		this._template = this._options.template;
+
+		if (this._options.shadowRender && !this.shadowRoot) {
+			this.attachShadow({ mode: 'open' });
+		}
+	}
+
+	template() {
+		return html``;
+	}
+
+	update(options) {
+		this.renderTemplate();
+		super.update(options);
+	}
+
+	renderTemplate() {
+		const template = this._template || this.template();
+
+		if (typeof template === 'string') {
+			// just a plain string literal. no fancy rendering required
+			this.getRoot().innerHTML = `${template}`;
+		} else {
+			render(template, this.getRoot());
+		}
+	}
+
+	getRoot() {
+		return this.shadowRoot !== null ? this.shadowRoot : this;
+	}
+}
+
+export { TemplateElement, html, unsafeHTML, render, spreadAttributes };
---
+++ src/renderer/uhtml/index.js
@@ -0,0 +1,18 @@
+import { BaseElement } from '../../BaseElement';
+import { StyledElement } from '../../StyledElement';
+import { TemplateElement, html, unsafeHTML, spreadAttributes } from './TemplateElement';
+import { toString } from '../../util/toString';
+import { defineElement } from '../../util/defineElement';
+import { i18n } from '../../util/i18n';
+
+export {
+	BaseElement,
+	StyledElement,
+	TemplateElement,
+	html,
+	unsafeHTML,
+	spreadAttributes,
+	toString,
+	defineElement,
+	i18n,
+};
