9825ac617b7222a7aaafbbe8dcb088a48358201c
Markus Hesper <mh@19f.de>
2022-10-26 23:14:16 +0200

    feat(StoreProperty): #38 typo

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ import { TemplateElement, html } from './src/TemplateElement';
 import { toString } from './src/util/toString';
 import { defineElement } from './src/util/defineElement';
 import { i18n } from './src/util/i18n';
-import { StoreProperty } from './src/util/PropertyStore';
+import { StoreProperty } from './src/util/StoreProperty';
 
 export { BaseElement, StyledElement, TemplateElement, StoreProperty, html, toString, defineElement, i18n };
