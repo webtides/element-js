b17c0b5fc88c84236f390fe58f932411ebbaac9d
Eddy Löwen <mail@eddyloewen.com>
2023-01-02 11:53:48 +0100

    removes two impossible todo comment nodes

--- src/renderer/vanilla/util/render.js
+++ src/renderer/vanilla/util/render.js
@@ -4,12 +4,8 @@
  * @param {Element} domNode
  */
 const render = (template, domNode) => {
-	// TODO: template could be a string ?!
-	// TODO: make it possible that template could also be an html element ?!
 	console.time('diff');
-
 	template.renderInto(domNode);
-
 	console.timeEnd('diff');
 };
 
