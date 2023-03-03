5f895b913ce97f7183755133c92546d887ddd344
Eddy Löwen <mail@eddyloewen.com>
2023-03-03 18:20:54 +0100

    adds comment for a bug

--- examples/example-test-element.js
+++ examples/example-test-element.js
@@ -18,17 +18,29 @@ class ExampleTestElement extends TemplateElement {
 
 	template() {
 		const list = [];
-		for (let i = 0; i <= this.count; i++) {
+		for (let i = 0; i < this.count; i++) {
 			list.push(i);
 		}
-		/*
-		<div foo="${this.foo}" bar="${this.bar}">
-				${this.text}
-				<div class="test">${list.map((item) => html` <div index="${item}">${this.text} - ${item}</div> `)}</div>
-			</div>
-		 */
+		/*<div class="test">
+			${list.map(
+				(item) =>
+					html`
+						<div index="${item}">${this.text}</div>
+						<div>${item}</div>
+					`,
+			)}
+		</div>*/
+		const p = document.createElement('p');
+		p.textContent = 'DOM Element';
+		// TODO: the nested TemplateResult ist not parsed or updated correctly
+		// it has a fragment, but no strings in ChildNodePart.parseTemplateResult()
+		// but the fragment only has the comment markers in it...
+		// there is probably a call to update missing?! or the fragment has to be parsed again?!
+		// but i think it is rather a missing call to update.
 		return html`
-			<div class="test">${list.map((item) => html` <div index="${item}">${this.text} - ${item}</div> `)}</div>
+			<div class="test2">
+				${[1, 'Text', html`<span>Foo</span>`, () => 'Function', p].map((item) => html` <div>${item}</div> `)}
+			</div>
 		`;
 	}
 }
