1f7c67fbf98c7f604099fac94820ad87e86996ae
Markus Hesper <mh@19f.de>
2023-03-07 23:07:13 +0100

    feat(store): #90 create Test to set expectation

--- test/unit/store.watch.test.js
+++ test/unit/store.watch.test.js
@@ -2,13 +2,23 @@
 import { fixture, defineCE, assert, nextFrame } from '@open-wc/testing';
 import { Store } from '../../src/util/Store';
 
+class NestedStore extends Store {
+	properties() {
+		return {
+			nestedCount: 0,
+		};
+	}
+}
+
 class WatchStore extends Store {
 	echoCount = -1;
 	echoOldCount = -1;
 	triggerCount = 0;
+	echoNestedStoreCount = 0;
 	properties() {
 		return {
 			count: 0,
+			nestedStore: new NestedStore(),
 		};
 	}
 	watch() {
@@ -17,6 +27,10 @@ class WatchStore extends Store {
 				this.echoCount = newCount;
 				this.echoOldCount = oldCount;
 				this.triggerCount++;
+			},
+			nestedStore: () => {
+				console.log(this.nestedStore.nestedCount);
+				this.echoNestedStoreCount = this.nestedStore.nestedCount;
 			},
 		};
 	}
@@ -44,5 +58,14 @@ describe('store-watch', () => {
 		// this should trigger the watcher as the value is changed
 		watchStore.count = 2;
 		assert.equal(watchStore.triggerCount, 2);
+	});
+
+	it('watches changes on nested store properties which are in itself instances of Store', async () => {
+		const watchStore = new WatchStore({ count: 0 });
+		assert.equal(watchStore.nestedStore.nestedCount, 0);
+
+		watchStore.nestedStore.nestedCount = 1;
+		assert.equal(watchStore.nestedStore.nestedCount, 1);
+		assert.equal(watchStore.echoNestedStoreCount, watchStore.nestedStore.nestedCount);
 	});
 });
