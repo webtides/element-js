bd4b7fca8164e56ac1c7a5db95525e46f3b8dcf5
Eddy Löwen <mail@eddyloewen.com>
2023-03-08 12:28:37 +0100

    adds comment for how to implement multiple attribute parts

--- src/renderer/vanilla/util/AttributePart.js
+++ src/renderer/vanilla/util/AttributePart.js
@@ -13,4 +13,11 @@ export class AttributePart extends Part {
 		this.name = name;
 		this.processor = processAttributePart(node.nextElementSibling, this.name);
 	}
+
+	// Maybe we can return an attribute part multiple times?
+	// and when calling the update method on the part, it will batch and only actually call the processor
+	// when all updates where set?!
+	// but how will the part know how many updates it has to wait for?
+	// it will probably have a string with the static values in it along with some kind of placeholders
+	// the amount of placeholders should be that number?
 }
