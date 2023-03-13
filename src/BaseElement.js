5099e581b225992fcd695ba6760651e8dd2291e1
quarkus <mh@19f.de>
2023-03-13 21:27:33 +0100

    feat(injection): still debugging shadow -> light injection. found anissue with connection order (requester connects before shadow provider)

--- src/BaseElement.js
+++ src/BaseElement.js
@@ -350,7 +350,7 @@ class BaseElement extends HTMLElement {
 		const providedKeys = Object.keys(this.provideProperties());
 		if (providedKeys.length > 0) {
 			// this.addEventListener('request-context', this.onRequestContext);
-			this.getRoot().addEventListener('request-context', this.onRequestContext);
+			this.getRoot().addEventListener('request-context', (e) => this.onRequestContext(e));
 			// check if there are already connected elements in child dom and restart requests
 			getAllElementChildren(this.getRoot(), BaseElement).forEach((customChild) => {
 				// if injectProperties?.() is defined it means that the child got connected BEFORE the parent (Runtime Issue)
