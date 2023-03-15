77462c3d67d89076bb2527e046c72960fd18d801
Eddy Löwen <mail@eddyloewen.com>
2023-03-15 12:13:52 +0100

    adds example that breaks arrays of mixed content

--- examples/example-test-element.js
+++ examples/example-test-element.js
@@ -1,5 +1,4 @@
 import { defineElement } from '../src/BaseElement';
-// import { TemplateElement, html } from '../src/TemplateElement';
 import { TemplateElement, html } from '../src/renderer/vanilla/TemplateElement.js';
 
 class ExampleTestElement extends TemplateElement {
@@ -21,27 +20,11 @@ class ExampleTestElement extends TemplateElement {
 		for (let i = 0; i < this.count; i++) {
 			list.push(i);
 		}
-		/*<div class="test">
-			${list.map(
-				(item) =>
-					html`
-						<div index="${item}">${this.text}</div>
-						<div>${item}</div>
-					`,
-			)}
-		</div>*/
 		const p = document.createElement('p');
 		p.textContent = 'DOM Element';
-		// TODO: the nested TemplateResult ist not parsed or updated correctly
-		// it has a fragment, but no strings in ChildNodePart.parseTemplateResult()
-		// but the fragment only has the comment markers in it...
-		// there is probably a call to update missing?! or the fragment has to be parsed again?!
-		// but i think it is rather a missing call to update.
 		return html`
-			<div class="test2">
-				${[1, 'Text', html`<span>Foo</span>`, html`<span>${this.count}</span>`, () => 'Function', p].map(
-					(item) => html` <div>${item}</div> `,
-				)}
+			<div class="test">
+				${[...list, 1, 'Text', html`<span>Foo</span>`, html`<span>${this.count}</span>`, () => 'Function', p]}
 			</div>
 		`;
 	}
