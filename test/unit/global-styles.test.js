cf30868b42c3982d78ae742e5241d05f71f90c41
Eddy Löwen <mail@eddyloewen.com>
2023-09-08 10:03:08 +0200

    adds tests for multiple global styles adoption

--- test/unit/global-styles.test.js
+++ test/unit/global-styles.test.js
@@ -1,10 +1,11 @@
 /* eslint-disable no-unused-expressions */
 import { fixture, defineCE, assert, expect, nextFrame, fixtureSync } from '@open-wc/testing';
-import { TemplateElement, html } from '../../src/TemplateElement';
-import { StyledElement } from '../../src/StyledElement';
+import { TemplateElement, html } from '../../src/TemplateElement.js';
 
 const color = 'rgb(255, 240, 0)';
 const color2 = 'rgb(255, 0, 255)';
+const color3 = 'rgb(0, 255, 0)';
+
 const shadowTag = defineCE(
 	class extends TemplateElement {
 		constructor() {
@@ -90,7 +91,7 @@ describe('global-styles', () => {
 		assert.equal(computeSpanColor, color2);
 	});
 
-	it('adopts globalStyles in shadowDom', async () => {
+	it('adopts all globalStyles in shadowDom', async () => {
 		const el = await fixture(`<${shadowTag}></${shadowTag}>`);
 		//this needs to be called to find the styles added ar runtime
 		await nextFrame();
@@ -99,6 +100,26 @@ describe('global-styles', () => {
 
 		const computeSpanColor = await window.getComputedStyle(el.$refs.coloredSpan).getPropertyValue('color');
 		assert.equal(computeSpanColor, color2);
+	});
+
+	it('adopts globalStyles in shadowDom when they get added asynchronously', async () => {
+		const el = await fixture(`<${shadowTag}></${shadowTag}>`);
+		//this needs to be called to find the styles added ar runtime
+		await nextFrame();
+		const computedColor = await window.getComputedStyle(el.$refs.coloredP).getPropertyValue('color');
+		assert.equal(computedColor, color);
+
+		const style = document.createElement('STYLE');
+		style.type = 'text/css';
+		style.id = 'globalStyles2';
+		style.appendChild(document.createTextNode(`p{color: ${color3}}`));
+		document.head.appendChild(style);
+
+		await nextFrame();
+		const computedColor3 = await window.getComputedStyle(el.$refs.coloredP).getPropertyValue('color');
+		assert.notEqual(computedColor3, color);
+		assert.equal(computedColor3, color3);
+		document.getElementById('globalStyles2').remove();
 	});
 
 	it('does not adopt globalStyles in shadowDom if option is false', async () => {
@@ -112,7 +133,7 @@ describe('global-styles', () => {
 		assert.equal(computeSpanColor, color2);
 	});
 
-	it('adopty styles in correct order (shadow-dom)', async () => {
+	it('adopts styles in correct order (shadow-dom)', async () => {
 		const el = fixtureSync(`<${shadowCascadeTag}></${shadowCascadeTag}>`);
 		await nextFrame();
 		const computedColor = await window.getComputedStyle(el.$refs.coloredP).getPropertyValue('color');
