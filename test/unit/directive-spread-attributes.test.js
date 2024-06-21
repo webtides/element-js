ae1ea018d8e22d86bb1f4a5bab6a7ab0c9eece77
Markus Hesper <mh@19f.de>
2024-06-21 14:04:48 +0200

    feat(attrs): enhance spreadAttribute to also remove attributes when set to null
    also adds an optionalAttribute directive to toggle the add/remove of a single attribute based on an external condition

--- test/unit/directive-spread-attributes.test.js
+++ test/unit/directive-spread-attributes.test.js
@@ -1,64 +1,40 @@
 /* eslint-disable no-unused-expressions */
 import { fixture, defineCE, assert } from '@open-wc/testing';
 import { html } from '../../src/TemplateElement.js';
-import { spreadAttributes, SpreadAttributesDirective } from '../../src/dom-parts/directives.js';
+import {
+    OptionalAttributeDirective,
+    spreadAttributes,
+    SpreadAttributesDirective,
+} from '../../src/dom-parts/directives.js';
 import { stripCommentMarkers } from './renderer/template-bindings.js';
 
 describe('spreadAttributes directive', () => {
-    it('maps primitive values to string attributes', async () => {
-        // SSR
-        const attributes = {
-            string: 'string',
-            number: 13,
-            boolean: true,
-        };
-        const templateResult = html`<div ${spreadAttributes(attributes)}></div>`;
-        assert.equal(
-            stripCommentMarkers(templateResult.toString()),
-            "<div string='string' number='13' boolean='true'></div>",
-        );
+    it('adds an attributes when condition is truthy', async () => {
+        let condition = true;
 
-        // CSR
         const el = document.createElement('div');
-        const directive = new SpreadAttributesDirective(el);
-        directive.update(attributes);
-        assert.equal(el.getAttribute('string'), 'string');
-        assert.equal(el.getAttribute('number'), '13');
-        assert.equal(el.getAttribute('boolean'), 'true');
+        const directive = new OptionalAttributeDirective(el);
+        directive.update(condition, 'attr', true);
+        assert.isTrue(el.hasAttribute('attr'));
+    });
+    it('does not add an attributes when condition is falsy', async () => {
+        let condition = false;
+
+        const el = document.createElement('div');
+        const directive = new OptionalAttributeDirective(el);
+        directive.update(condition, 'attr', true);
+        assert.isFalse(el.hasAttribute('attr'));
     });
 
-    it('maps object like values to encoded and JSON parsable attributes', async () => {
-        // SSR
-        const attributes = {
-            list: [1, '2', 3],
-            map: { foo: 'bar' },
-        };
-        const templateResult = html`<div ${spreadAttributes(attributes)}></div>`;
-        assert.equal(
-            stripCommentMarkers(templateResult.toString()),
-            "<div list='[1,&quot;2&quot;,3]' map='{&quot;foo&quot;:&quot;bar&quot;}'></div>",
-        );
+    it('does add and remove an attributes when condition is toggled', async () => {
+        let condition = true;
 
-        // CSR
         const el = document.createElement('div');
-        const directive = new SpreadAttributesDirective(el);
-        directive.update(attributes);
-        assert.equal(el.getAttribute('list'), '[1,&quot;2&quot;,3]');
-        assert.equal(el.getAttribute('map'), '{&quot;foo&quot;:&quot;bar&quot;}');
-    });
-
-    it('converts camelCase names to dash-case', async () => {
-        // SSR
-        const attributes = {
-            camelToDash: 'automagically',
-        };
-        const templateResult = html`<div ${spreadAttributes(attributes)}></div>`;
-        assert.equal(stripCommentMarkers(templateResult.toString()), "<div camel-to-dash='automagically'></div>");
-
-        // CSR
-        const el = document.createElement('div');
-        const directive = new SpreadAttributesDirective(el);
-        directive.update(attributes);
-        assert.equal(el.getAttribute('camel-to-dash'), 'automagically');
+        const directive = new OptionalAttributeDirective(el);
+        directive.update(condition, 'attr', true);
+        assert.isTrue(el.hasAttribute('attr'));
+        condition = false;
+        directive.update(condition, 'attr', true);
+        assert.isFalse(el.hasAttribute('attr'));
     });
 });
