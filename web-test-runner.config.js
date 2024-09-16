47c5cb0e985b40705ca3841254010d931a3d260d
Markus Hesper <mh@19f.de>
2024-09-16 17:24:16 +0200

    fix(tests): tmp fix to work around the timing issues between chrome (v128) and the test runner
    https://github.com/open-wc/open-wc/issues/2813#issuecomment-2317609810

--- web-test-runner.config.js
+++ web-test-runner.config.js
@@ -1,4 +1,11 @@
+import { chromeLauncher } from '@web/test-runner';
+
 export default {
+    browsers: [
+        chromeLauncher({
+            concurrency: 1, // https://github.com/open-wc/open-wc/issues/2813#issuecomment-2317609810
+        }),
+    ],
     coverageConfig: {
         reportDir: 'test/coverage',
     },
