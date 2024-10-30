106bec694330d56b343c39bda58bcbc7afefca82
Markus Hesper <mh@19f.de>
2024-10-30 19:57:05 +0100

    fix(131): adds failing tests with simple reproduction

--- test/unit/vanilla-renderer-updates.test.js
+++ test/unit/vanilla-renderer-updates.test.js
@@ -8,20 +8,16 @@ class LoadingElement extends TemplateElement {
         };
     }
 
-    templateText() {
-        return html`<p>loading ...</p>`;
-    }
-
     template() {
         console.log('load prop:', this.load);
-        return html` ${this.loading ? this.templateText() : 'ready'} `;
+        return html` ${this.loading ? html`<p>loading ...</p>` : 'ready'} `;
     }
 }
 
 customElements.define('loading-element', LoadingElement);
 
 describe(`vanilla-renderer-updates`, () => {
-    it('should rerenderand update an element event if the template is nested in a dedicated function', async () => {
+    it('should rerender a nested template/html literal even if this is in a ternary ', async () => {
         const el = await fixture(`<loading-element></loading-element>`);
         await el.requestUpdate();
         assert.isTrue(el.innerText.includes('loading'));
