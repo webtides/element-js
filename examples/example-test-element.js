d1b70525fe752f0c4d6702e52256cad70eb071aa
Eddy Löwen <mail@eddyloewen.com>
2025-03-13 17:47:51 +0100

    adds todos

--- examples/example-test-element.js
+++ examples/example-test-element.js
@@ -1,5 +1,6 @@
 import { defineElement } from '../src/BaseElement.js';
 import { TemplateElement, html } from '../src/TemplateElement.js';
+import { choose } from '../src/dom-parts/directives.js';
 
 class ExampleTestElement extends TemplateElement {
     constructor() {
@@ -18,81 +19,106 @@ class ExampleTestElement extends TemplateElement {
         };
     }
 
-    renderState(states) {
-        if (this.state === 'initial') return null;
-        return states[this.state];
-    }
-
     template() {
-        const list = [];
-        for (let i = 0; i < this.count; i++) {
-            list.push(i);
+        const example = new URLSearchParams(window.location.search).get('example');
+
+        if (example === '1') {
+            const p = document.createElement('p');
+            p.textContent = 'DOM Element';
+            // rendering different things inside arrays
+            return html`
+                <div class="test">
+                    ${[
+                        this.count,
+                        1,
+                        'Text',
+                        html`<span>Foo</span>`,
+                        html`<span>${this.count}</span>`,
+                        () => 'Function',
+                        p,
+                    ]}
+                </div>
+            `;
         }
-        const p = document.createElement('p');
-        p.textContent = 'DOM Element';
-        // return html`
-        // 	<div class="test">
-        // 		${[this.count, 1, 'Text', html`<span>Foo</span>`, html`<span>${this.count}</span>`, () => 'Function', p]}
-        // 	</div>
-        // `;
-        // return html`<div>${html`<strong>${this.text}</strong>`}</div>`;
-        // return html`
-        // 	<ul>
-        // 		${list.map((item) => html`<li>${item}</li>`)}
-        // 	</ul>
-        // `;
-        // return html`
-        // 	<div>
-        // 		${list.map((item) => item)}
-        // 	</div>
-        // `;
 
-        // return html`<ul ref="list">
-        //     ${this.list.map((index) => html` <li>${index}</li>`)}
-        // </ul>`;
+        if (example === '2') {
+            return html`<div>${html`<strong>${this.text}</strong>`}</div>`;
+        }
 
-        // return html`<div>
-        //     ${this.renderArray
-        //         ? html`<ul ref="list" data-length="${this.list.length}">
-        //               ${this.list.map((index) => html` <li>${index}</li>`)}
-        //           </ul>`
-        //         : html`<strong>no list</strong>`}
-        // </div>`;
-        //
-        // const templateFn = () => {
-        //     if (this.renderArray) {
-        //         return html`<ul ref="list" data-length="${this.list.length}">
-        //             ${this.list.map((index) => html` <li>${index}</li>`)}
-        //         </ul>`;
-        //     }
-        //     return html`<strong>no list</strong>`;
-        // };
-
-        const templateFn2 = () => {
-            if (this.renderArray) {
-                return this.list.map((index) => html` <li>${index}</li>`);
+        if (example === '3') {
+            const list = [];
+            for (let i = 0; i < this.count; i++) {
+                list.push(i);
             }
-            return html`<strong>no list</strong>`;
-        };
 
-        return html`<div>${templateFn2()}</div>`;
+            return html`
+                <ul>
+                    ${list.map((item) => html`<li>${item}</li>`)}
+                </ul>
+            `;
+        }
 
-        return html`
-            <div>
-                ${this.renderState({
-                    loading: html`
-                        <div class="bg-gray-500 p-4">
-                            <p class="m-0">Loading ...</p>
-                        </div>
-                    `,
-                    result: html`
-                        <div class="bg-gray-500 p-4">
-                            <p class="m-0">Result</p>
-                        </div>
-                    `,
-                })}
-            </div>
-        `;
+        if (example === '4') {
+            const list = [];
+            for (let i = 0; i < this.count; i++) {
+                list.push(i);
+            }
+            return html` <div>${list.map((item) => item)}</div> `;
+        }
+
+        if (example === '5') {
+            return html`<div>
+                ${this.renderArray
+                    ? html`<ul ref="list" data-length="${this.list.length}">
+                          ${this.list.map((index) => html` <li>${index}</li>`)}
+                      </ul>`
+                    : html`<strong>no list</strong>`}
+            </div>`;
+        }
+
+        if (example === '6') {
+            const templateFn = () => {
+                if (this.renderArray) {
+                    return html`<ul ref="list" data-length="${this.list.length}">
+                        ${this.list.map((index) => html` <li>${index}</li>`)}
+                    </ul>`;
+                }
+                return html`<strong>no list</strong>`;
+            };
+
+            return html`<div>${templateFn()}</div>`;
+        }
+
+        if (example === '7') {
+            const templateFn2 = () => {
+                if (this.renderArray) {
+                    return this.list.map((index) => html` <li>${index}</li>`);
+                }
+                return html`<strong>no list</strong>`;
+            };
+
+            return html`<div>${templateFn2()}</div>`;
+        }
+
+        if (example === '8') {
+            return html`
+                <div>
+                    ${choose(this.state,{
+                        loading: html`
+                            <div class="bg-gray-500 p-4">
+                                <p class="m-0">Loading ...</p>
+                            </div>
+                        `,
+                        result: html`
+                            <div class="bg-gray-500 p-4">
+                                <p class="m-0">Result</p>
+                            </div>
+                        `,
+                    }, null)}
+                </div>
+            `;
+        }
+
         // prettier-ignore
         // return html` <div foo="${'bar'}" bar='${'baz'}' baz=${'blup'} class="link active disabled"></div> `;
         // return html`${this.text}`;
