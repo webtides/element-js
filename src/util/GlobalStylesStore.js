5c5696bbad678ced684738001f246a5a382a4323
Markus Hesper <mhesper@dev5310.com>
2025-06-10 22:57:29 +0200

    no-ticket: actually fill cache of GlobalStylesStore.js
    split create and process function
    adds timing logs and await to actually measuer the performance of the options ()originall ist fastest for me in chromium.

--- src/util/GlobalStylesStore.js
+++ src/util/GlobalStylesStore.js
@@ -9,86 +9,115 @@ class GlobalStylesStore extends Store {
 
     constructor() {
         super();
+        this.createGlobalStyleSheetsCache();
         if (globalThis.elementJsConfig?.observeGlobalStyles) {
-            const mutationObserver = new MutationObserver((mutationRecord) => {
-                if (!mutationRecord[0]) return;
-                const filteredNodes = Array.from(mutationRecord[0].addedNodes).filter(
-                    (node) => node.tagName === 'STYLE' || node.tagName === 'LINK',
-                );
-                if (filteredNodes && filteredNodes[0] && filteredNodes[0].tagName === 'LINK') {
-                    filteredNodes[0].onload = () => {
-                        // TODO maybe its possible to simply aff the node aka sytlesheet !?
-                        this.createGlobalStyleSheetsCache();
-                        this.requestUpdate();
-                    };
-                } else if (filteredNodes && filteredNodes[0] && filteredNodes[0].tagName === 'STYLE') {
-                    this.createGlobalStyleSheetsCache();
-                    this.requestUpdate();
+            this.initMutationObserver();
+        }
+    }
+
+    initMutationObserver() {
+        const mutationObserver = new MutationObserver(async (mutationRecord) => {
+            if (!mutationRecord[0]) return;
+            const filteredNodes = Array.from(mutationRecord[0].addedNodes).filter(
+                (node) => node.tagName === 'STYLE' || node.tagName === 'LINK',
+            );
+
+            filteredNodes.forEach((styleNode) => {
+                // TODO WTF !? this has to be done for every node .. not only the first !?
+                if (styleNode.tagName === 'LINK') {
+                    if (styleNode.sheet) {
+                        // already loaded
+                        this.processStylesheet(styleNode);
+                    } else {
+                        // wait for download
+                        styleNode.onload = () => {
+                            this.processStylesheet(styleNode);
+                        };
+                    }
+                } else if (styleNode.tagName === 'STYLE') {
+                    this.processStylesheet(styleNode);
                 }
             });
-            mutationObserver.observe(globalThis.document, { subtree: true, childList: true });
+        });
+        mutationObserver.observe(globalThis.document, { subtree: true, childList: true });
+    }
+
+    async processStylesheet(styleSheet) {
+        console.time('createGlobalStyleSheetsCache');
+
+        let cssStyleSheet = this.globalStyleSheetsCache.get(styleSheet.ownerNode);
+        if (!cssStyleSheet) {
+            // if it does not exist yet -> build it
+            if (styleSheet.ownerNode.tagName === 'STYLE') {
+                const cssStyleSheet = new CSSStyleSheet({ media: styleSheet.media, disabled: styleSheet.disabled });
+                console.timeLog('createGlobalStyleSheetsCache', `## Style 1: ${styleSheet.href}`);
+                cssStyleSheet.replaceSync(styleSheet.ownerNode.textContent);
+                console.timeLog('createGlobalStyleSheetsCache', `## Style 2: ${styleSheet.href}`);
+
+                this.globalStyleSheetsCache.set(styleSheet.ownerNode, cssStyleSheet);
+            } else if (styleSheet.ownerNode.tagName === 'LINK') {
+                const cssStyleSheet = new CSSStyleSheet({
+                    baseURL: styleSheet.href,
+                    media: styleSheet.media,
+                    disabled: styleSheet.disabled,
+                });
+                try {
+                    console.timeLog('createGlobalStyleSheetsCache', `## LINK 1: ${styleSheet.href}`);
+                    // TODO this is like super expensive... - approx 22ms
+                    // original version
+                    Array.from(styleSheet?.cssRules ?? []).map((rule, index) =>
+                        cssStyleSheet.insertRule(rule.cssText, index),
+                    );
+
+                    // TODO: this is a bit faster... but still expensive - approx 20ms
+                    // TODO: slowser for me in chrome ... more like 56ms
+                    // Get all CSS text in one go
+                    // const allCssText = Array.from(styleSheet.cssRules)
+                    //     .map((rule) => rule.cssText)
+                    //     .join('\n');
+                    // // Apply all at once
+                    // cssStyleSheet.replace(allCssText);
+
+                    // TODO im not sure i like this, but i would assume that the request is cached already?! - approx 1ms
+                    // Fetch the CSS content directly from the href
+                    // todo -> nope this is by far the slowest (250ms) and moves the adoption to the very end.
+                    // try {
+                    //     const response = await fetch(styleSheet.href);
+                    //     const cssText = await response.text();
+                    //
+                    //     // console.info('GlobalStylesStore:cssText', cssText);
+                    //
+                    //     await cssStyleSheet.replace(cssText);
+                    // } catch (e) {
+                    //     console.error('GlobalStylesStore: Cannot fetch stylesheet content', e);
+                    // }
+
+                    this.globalStyleSheetsCache.set(styleSheet.ownerNode, cssStyleSheet);
+
+                    console.log('## added to the cache: ', cssStyleSheet);
+                    console.timeLog('createGlobalStyleSheetsCache', `## LINK AFTER: ${styleSheet.href}`);
+                    // update observer that there might be new styles to adopt
+                    this.requestUpdate();
+                } catch (e) {
+                    console.error(
+                        'GlobalStylesStore: cannot read cssRules. Maybe add crossorigin="anonymous" to your style link?',
+                        e,
+                    );
+                }
+            }
+
+            console.log('#######    ################');
+            console.timeEnd('createGlobalStyleSheetsCache');
+            console.log(`#######    ${styleSheet.ownerNode}`);
+            console.log(`#######    rules      :: ${styleSheet.rules.length}`);
+            console.log('#######    ################');
         } else {
-            this.createGlobalStyleSheetsCache();
+            console.log('GlobalStylesStore: cache hit,  already processed', styleSheet);
         }
     }
 
     createGlobalStyleSheetsCache() {
-        // TODO create early return in node is already in
-        console.time('createGlobalStyleSheetsCache');
-        console.timeLog('createGlobalStyleSheetsCache', 'BUILD OR REBUILD');
-        Array.from(globalThis.document?.styleSheets).map((styleSheet) => {
-            let cssStyleSheet = this.globalStyleSheetsCache.get(styleSheet.ownerNode);
-            if (!cssStyleSheet) {
-                // if it does not exist yet -> build it
-                if (styleSheet.ownerNode.tagName === 'STYLE') {
-                    const cssStyleSheet = new CSSStyleSheet({ media: styleSheet.media, disabled: styleSheet.disabled });
-                    cssStyleSheet.replaceSync(styleSheet.ownerNode.textContent);
-
-                    this.globalStyleSheetsCache.set(styleSheet.ownerNode, cssStyleSheet);
-                } else if (styleSheet.ownerNode.tagName === 'LINK') {
-                    const cssStyleSheet = new CSSStyleSheet({
-                        baseURL: styleSheet.href,
-                        media: styleSheet.media,
-                        disabled: styleSheet.disabled,
-                    });
-                    try {
-                        console.timeLog('createGlobalStyleSheetsCache', '## 1');
-                        // TODO this is like super expensive... - approx 22ms
-                        // original version
-                        // Array.from(styleSheet?.cssRules ?? []).map((rule, index) =>
-                        //     cssStyleSheet.insertRule(rule.cssText, index),
-                        // );
-
-                        // TODO: this is a bit faster... but still expensive - approx 20ms
-                        // Get all CSS text in one go
-                        // const allCssText = Array.from(styleSheet.cssRules)
-                        //     .map((rule) => rule.cssText)
-                        //     .join('\n');
-                        // // Apply all at once
-                        // cssStyleSheet.replaceSync(allCssText);
-
-                        // TODO im not sure i like this, but i would assume that the request is cached already?! - approx 1ms
-                        // Fetch the CSS content directly from the href
-                        fetch(styleSheet.href)
-                            .then((response) => response.text())
-                            .then((cssText) => {
-                                cssStyleSheet.replaceSync(cssText);
-                            })
-                            .catch((e) => {
-                                console.error('GlobalStylesStore: Cannot fetch stylesheet content', e);
-                            });
-
-                        console.timeLog('createGlobalStyleSheetsCache', '## 2');
-                        this.globalStyleSheetsCache.set(styleSheet.ownerNode, cssStyleSheet);
-                    } catch (e) {
-                        console.error(
-                            'GlobalStylesStore: cannot read cssRules. Maybe add crossorigin="anonymous" to your style link?',
-                            e,
-                        );
-                    }
-                }
-            }
-        });
+        Array.from(globalThis.document?.styleSheets).map((styleSheet) => this.processStylesheet(styleSheet));
     }
 
     getGlobalStyleSheets(selector) {
